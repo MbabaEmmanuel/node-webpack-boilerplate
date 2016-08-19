e7f1423ac4ad255f2165b1ef48d9fc5cca3d6d77
Peter Zastoupil <peterzastoupil@gmail.com>
2016-08-19 15:03:13 -0500

    Initial import.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  target: 'node',
+
+  entry: {
+    app: ['babel-polyfill', './src/app.js']
+  },
+
+  output: {
+    path: './dist/',
+    filename: '[name].js'
+  },
+
+  module: {
+    loaders: [{
+      test: /.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  }
+}
